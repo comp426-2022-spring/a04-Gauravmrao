215256fa9c11d345b53fe96605cafbd6270c2258
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-04-27 14:20:28 -0400

    fixed typos

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const database = require('better-sqlite3')
 const logdb = new database('log.db')
 
 // define statement
-const statement = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='access';`)
+const statement = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='access';`)
 
 // define row
 let row = statement.get();
